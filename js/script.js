f1846615fde131bad91eda99c6edb8b43f51b146
Danny Vega <dannyevega@gmail.com>
2015-10-28 12:41:09 -0700

    search now only shows relevant thumbnail results for each search and clears the input box each time

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 var $form = $('#search-term');
+var $searchQuery = $('#query');
 
 $form.submit(function(el){
 	el.preventDefault();
-	var $searchQuery = $('#query').val();
-	getRequest($searchQuery);
+	var searchQuery = $searchQuery.val();
+	getRequest(searchQuery);
 });
 
-var getRequest = (function(userInput){
+var getRequest = (function(){
 	var params = {
 		part: 'snippet',
 		key: "AIzaSyAXZYwp-w3D5_n1NXgfGf1PffG3fxxBZvs"
@@ -17,7 +18,8 @@ var getRequest = (function(userInput){
 		params.q = userInput;
 		$.getJSON(url, params, function(data){
 			var thumbnailData = data.items;
-			appendThumbnails(thumbnailData);		
+			appendThumbnails(thumbnailData);	
+			$searchQuery.val('');
 		});		
 	};
 
@@ -25,34 +27,19 @@ var getRequest = (function(userInput){
 
 var appendThumbnails = (function(){
 	var $thumbnailList = $('#search-results');
-	var thumbnailMarkUp = "";
 
-	return function(data){
-		for(var i = 0; i < data.length; i++){
-			thumbnailMarkUp += '<p><img src="' + data[i].snippet.thumbnails.medium.url + '"></p>';			
-			$thumbnailList.html(thumbnailMarkUp);				
-		}	
-	};
+	return function(items){
+		var elements = [];
+		for(var i = 0; i < items.length; i++){
+			var $p = $('<p>');
+			var $img = $('<img>').attr('src', items[i].snippet.thumbnails.medium.url);
+			$p.append($img);
+			elements.push($p);
+		}
+		$thumbnailList.html(elements);
+	}
 
 })();
-
-
-// var getRequest = (function(userInput){
-// 	var params = {
-// 		part: 'snippet',
-// 		key: "AIzaSyAXZYwp-w3D5_n1NXgfGf1PffG3fxxBZvs",
-// 		q: userInput
-// 	};
-// 	url = "https://www.googleapis.com/youtube/v3/search";
-
-// 	return function(){
-// 		$.getJSON(url, params, function(data){
-// 			var thumbnailData = data.items;
-// 			appendThumbnails(thumbnailData);		
-// 		});		
-// 	};
-
-// })();
 
 // function getRequest(userInput){
 // 	var params = {
@@ -67,3 +54,17 @@ var appendThumbnails = (function(){
 // 		appendThumbnails(thumbnailData);		
 // 	});
 // }
+
+
+// var appendThumbnails = (function(){
+// 	var $thumbnailList = $('#search-results');
+// 	var thumbnailMarkUp = "";
+
+// 	return function(data){
+// 		for(var i = 0; i < data.length; i++){
+// 			thumbnailMarkUp += '<p><img src="' + data[i].snippet.thumbnails.medium.url + '"></p>';			
+// 			$thumbnailList.html(thumbnailMarkUp);				
+// 		}	
+// 	};
+
+// })();
